5712df84e42dd6d15a856fa602f341d160a0fdeb
Frederic Bertels <frederic.bertels@gmail.com>
2013-07-27 00:37:13 +1200

    2nd test

--- RealPhy_merge/src/buildTreesFromSequenceData/RealPhy.java
+++ RealPhy_merge/src/buildTreesFromSequenceData/RealPhy.java
@@ -14,7 +14,7 @@ import util.*;
 
 public class RealPhy {
 	/**
-	 * Now with version control!
+	 * Now with version control! Tester2
 	 */
 	private static final long serialVersionUID = 101L;
 	public static  final String fasExt[]=new String[]{"fas","fa","fasta","fna"};
