2be52fa264b765c435c13f92df22864287c4f5cc
Frederic Bertels <frederic.bertels@gmail.com>
2014-05-09 11:40:58 +0200

    Added licence agreement to RealPhy.java

--- RealPhy_merge/src/buildTreesFromSequenceData/RealPhy.java
+++ RealPhy_merge/src/buildTreesFromSequenceData/RealPhy.java
@@ -10,8 +10,23 @@ import rcaller.*;
 import util.phylogenetics.RunTreePrograms;
 import util.*;
 
+/*
+REALPHY, a program to automatically reconstruct phylogenetic trees from short sequence reads. 
 
+Copyright (C) 2014 Frederic Bertels
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
 
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 public class RealPhy {
 	
 	static String version="v1.08";
