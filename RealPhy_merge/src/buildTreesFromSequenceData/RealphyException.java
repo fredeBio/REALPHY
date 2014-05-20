911039b4a1d8a07ac4bb65615ca3826b36dc90d2
Frederic Bertels <frederic.bertels@gmail.com>
2014-05-20 10:57:34 +0200

    New Class for Exceptions.

---
+++ RealPhy_merge/src/buildTreesFromSequenceData/RealphyException.java
@@ -0,0 +1,8 @@
+package buildTreesFromSequenceData;
+
+public class RealphyException extends Exception {
+	static final long serialVersionUID=1;
+  public RealphyException(String error){
+	  super(error);
+  }
+}
